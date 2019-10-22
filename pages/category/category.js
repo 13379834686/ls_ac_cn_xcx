4c3bb8bae865bd6405d280abec8b6bb0b5289255
shannon <eamil@qq.com>
2019-10-22 11:47:42 +0800

    小程序前端--完成首页布局--完成右侧导航栏列表

--- pages/category/category.js
+++ pages/category/category.js
@@ -7,32 +7,82 @@ Page({
   data: {
     activeId: '1',
 
-    typeList:[
-      { 'id': '1', 'title':' 分类1'},
-      { 'id': '2', 'title': '分类2' },
-      { 'id': '3', 'title': '分类3' },
-      { 'id': '4', 'title': '分类4' },
-      { 'id': '5', 'title': '分类5' },
-      { 'id': '6', 'title': '分类6' },
-      { 'id': '7', 'title': '分类6' }
+    typeList: [{
+        'id': '1',
+        'title': ' 分类1'
+      },
+      {
+        'id': '2',
+        'title': '分类2'
+      },
+      {
+        'id': '3',
+        'title': '分类3'
+      },
+      {
+        'id': '4',
+        'title': '分类4'
+      },
+      {
+        'id': '5',
+        'title': '分类5'
+      },
+      {
+        'id': '6',
+        'title': '分类6'
+      },
+      {
+        'id': '7',
+        'title': '分类6'
+      }
+    ],
+    goodsList: [{
+        'id': '1',
+        'title': '商品1',
+        'img': '/image/cg1.jpg'
+      },
+      {
+        'id': '2',
+        'title': '商品2',
+        'img': '/image/cg1.jpg'
+      },
+      {
+        'id': '3',
+        'title': '商品3',
+        'img': '/image/cg1.jpg'
+      },
+      {
+        'id': '4',
+        'title': '商品4',
+        'img': '/image/cg1.jpg'
+      },
+
     ]
 
   },
- 
+
   /**
    * 生命周期函数--监听页面加载
    */
-  onload: function () {
-    
+  onload: function() {
+
   },
 
-   selectType(e){
+  selectType(e) {
     // 点击获取当前选中id
     //  console.log(e.currentTarget.dataset.id)
-     this.setData({
-       activeId : e.currentTarget.dataset.id
-     })
-     console.log(e.currentTarget.dataset.id)
+    this.setData({
+      activeId: e.currentTarget.dataset.id
+    })
+  },
 
+  // 通过navigateTo实现商品跳转
+  goodDatail(e){
+    // url = "../detail/detail?id={{item.id}}"
+    // console.log(e.currentTarget.dataset.id)
+    wx.navigateTo({
+      url: '../gdetail/gdetail?id=' + e.currentTarget.dataset.id,
+    })
   }
+
 })
---
+++ pages/gdetail/gdetail.js
@@ -0,0 +1,70 @@
+// pages/gdetail/gdetail.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    goodsid:-1
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   * 获取商品传递过来的id  以goodsid接收
+   */
+  onLoad: function (options) {
+    console.log(options)
+    this.setData({
+      goodsid: options.id
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
